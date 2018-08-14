9ef80fa5a141b60cafe19ed002cd07472bc7b28c
Anna Sakowska <annsak2016@gmail.com>
2018-08-14 15:47:34 +0200

    Add components

--- book-tracking-app/src/App.js
+++ book-tracking-app/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Search from './components/Search.js'
+import Books from './components/Books.js'
+import * as BooksAPI from './components/BooksApi.js'
+
 
 class App extends Component {
+  state = {
+      myReads: []
+  }
+
+  componentDidMount() {
+      BooksAPI.getAll().then(myReads => {
+          this.setState({ myReads })
+      })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Search />
+        <Books />
       </div>
     );
   }
